f380da9faefb5a90d7462a25eaafed04d175e5b6
Gustavo S. Fernandes <marshall@arch.localdomain>
2021-04-14 15:36:07 -0300

    Lists update

--- src/pages/setup/script.js
+++ src/pages/setup/script.js
@@ -1,32 +1,46 @@
 const api = require('../../service/api')
 
 function handle_save() {
-    const question = document.querySelector('textarea#txt_question').value
-    const alt_a = document.querySelector('textarea#txt_alt_a').value
-    const alt_b = document.querySelector('textarea#txt_alt_b').value
-    const alt_c = document.querySelector('textarea#txt_alt_c').value
-    const alt_d = document.querySelector('textarea#txt_alt_d').value
-    const answer = document.querySelector('select#cmb_answer').value
-    const team = document.querySelector('select#cmb_team').value
+    const question = document.querySelector('textarea#txt_question')
+    const alt_a = document.querySelector('textarea#txt_alt_a')
+    const alt_b = document.querySelector('textarea#txt_alt_b')
+    const alt_c = document.querySelector('textarea#txt_alt_c')
+    const alt_d = document.querySelector('textarea#txt_alt_d')
+    const answer = document.querySelector('select#cmb_answer')
+    const team = document.querySelector('select#cmb_team')
 
-    const fields = [question, alt_a, alt_b, alt_c, alt_d]
+    const fields = [
+        question.value,
+        alt_a.value,
+        alt_b.value,
+        alt_c.value,
+        alt_d.value
+    ]
 
     if (check_fields(fields)) {
         if (confirm('Deseja cadastrar essa pergunta?')) {
             try {
                 const data = {
-                    "pergunta": question,
-                    "alt_a": alt_a,
-                    "alt_b": alt_b,
-                    "alt_c": alt_c,
-                    "alt_d": alt_d,
-                    "resposta": answer,
-                    "equipe": team
+                    "pergunta": question.value,
+                    "alt_a": alt_a.value,
+                    "alt_b": alt_b.value,
+                    "alt_c": alt_c.value,
+                    "alt_d": alt_d.value,
+                    "resposta": answer.value,
+                    "equipe": team.value
                 }
 
                 api.post('game', data).then((response) => {
                     if (response.status === 201) {
                         alert('Pergunta cadastrada com sucesso!')
+
+                        question.value = ''
+                        alt_a.value = ''
+                        alt_b.value = ''
+                        alt_c.value = ''
+                        alt_d.value = ''
+
+                        question.focus()
                     } else {
                         alert('Algo deu errado, tente novamente.')
                     }
@@ -50,7 +64,7 @@ function check_fields(fields) {
     return true
 }
 
-function handle_spreadsheet() {
+function handle_list() {
     try {
         api.get('questions').then((response) => {
             const fs = require('fs')
@@ -62,7 +76,7 @@ function handle_spreadsheet() {
                 fs.writeFileSync(`${os}/Documentos/perguntas.json`, JSON.stringify(response.data))
             }
 
-            alert('JSON gerado com sucesso')
+            alert('Lista de perguntas [perguntas.json] gerada com sucesso na pasta [Documentos]')
         })
     } catch (error) {
         alert(error)
